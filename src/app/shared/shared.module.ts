30b6cbc4a7d35ad11dda8be7cce9a89f141b54e1
aimide <teerawutc4@gmail.com>
2020-07-07 12:13:47 +0700

    Directive Parallax

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NumberOnlyDirective } from './directives/number-only.directive';
 import { ThaiDatePipe } from './pipes/thai-date.pipe';
+import { ParallaxDirective } from './directives/parallax.directive';
 
 
 
 @NgModule({
   declarations: [
     NumberOnlyDirective,
-    ThaiDatePipe
+    ThaiDatePipe,
+    ParallaxDirective
   ],
   imports: [
     CommonModule
